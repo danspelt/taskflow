5d4881cce27ffc6f136d58bbdb2e3e67fe8070b3
dansneil <62000481+danspelt@users.noreply.github.com>
2024-03-12 11:50:23 -0700

    Refactor Menu and TaskCard components

--- app/components/NavBar/Menu.jsx
+++ app/components/NavBar/Menu.jsx
@@ -1,31 +1,36 @@
-'use client';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useAuth } from '@/context/AuthContext';
-import { useAppContext } from '@/context/AppContext';
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useAuth } from "@/context/AuthContext";
+import { useAppContext } from "@/context/AppContext";
 const Menu = () => {
-  const { isProfileMenuOpen } = useAppContext(); 
+  const { isProfileMenuOpen } = useAppContext();
   const { currentUser, logout } = useAuth();
   // Apply conditional classes based on isOpen prop
   const menuClasses = isProfileMenuOpen ? "block" : "hidden";
 
   return (
     <div className={`absolute right-0 ${menuClasses}`}>
-      <div className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content shadow-lg">      
+      <div className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content shadow-lg">
         <li className="border-b-2 border-gray">
           <Link href="/profile">
-          <Image
-            src={currentUser.photoURL}
-            alt={currentUser.displayName}
-            width={40}
-            height={40}
-            className="rounded-full"
-          />
-          Profile          
-        </Link>
-        </li>        
+            <Image
+              src={currentUser.photoURL}
+              alt={currentUser.displayName}
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            Profile
+          </Link>
+        </li>
         <li>
-           <button onClick={() => logout()} className="btn btn-ghost">Logout</button> 
+          <Link href="/todos">My Todos</Link>
+        </li>
+        <li>
+          <button onClick={() => logout()} className="btn btn-ghost">
+            Logout
+          </button>
         </li>
       </div>
     </div>
--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
-import { Task } from '@/types';
-import { format, isPast } from 'date-fns';
+import React from "react";
+import { Task } from "@/types";
+import { format, isPast } from "date-fns";
 
 interface TaskCardProps {
   task: Task;
 }
 
-const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
-  // Format the deadline date or show "No deadline" if not set
-  const deadlineFormatted = task.deadline ? format(task.deadline.toDate(), 'PPP') : 'No deadline';
-  const isDeadlinePast = task.deadline ? isPast(task.deadline.toDate()) : false;
+const TaskCard: React.FC<TaskCardProps> = ({
+  task: { title, priority, deadline },
+}) => {
+  // Check if deadline is already a Date object or if it's a string that needs parsing
+  const deadlineDate = deadline
+    ? deadline instanceof Date
+      ? deadline
+    : new Date(deadline as string)
+    : null;
+  const deadlineFormatted = deadlineDate
+    ? format(deadlineDate, "MMM d, yyyy")
+    : "No deadline";
+  const isDeadlinePast = deadlineDate ? isPast(deadlineDate) : false;
 
   return (
-    <div className={`p-4 rounded-lg shadow-md ${isDeadlinePast ? 'bg-red-100' : 'bg-white'}`}>
-      <h3 className="text-lg font-bold">{task.title}</h3>
-      <p>Priority: {task.priority}</p>
-      <p>Deadline: <span className={`${isDeadlinePast ? 'text-red-600' : 'text-gray-800'}`}>{deadlineFormatted}</span></p>
+    <div
+      className={`p-4 rounded-lg shadow-md ${
+        isDeadlinePast ? "bg-red-100" : "bg-white"
+      }`}
+    >
+      <h3 className="text-lg font-bold">{title}</h3>
+      <dl>
+        <dt className="font-medium">Priority:</dt>
+        <dd>{priority}</dd>
+        <dt className="font-medium">Deadline:</dt>
+        <dd className={`${isDeadlinePast ? "text-red-600" : "text-gray-800"}`}>
+          {deadlineFormatted}
+        </dd>
+      </dl>
     </div>
   );
 };
